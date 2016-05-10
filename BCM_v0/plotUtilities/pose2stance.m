7b17d1a6a1c48e3eb7120c0e1c4ef09dcf50eb23
dexter0us <uetian.dawer@gmail.com>
2016-05-10 22:25:58 +0500

    some comments and other things

--- BCM_v0/plotUtilities/pose2stance.m
+++ BCM_v0/plotUtilities/pose2stance.m
@@ -4,7 +4,16 @@
 % ali.raza@ymail.com
 %
 % University of Engineering and Technology
+
+% input variables:
+% x= x-coordinate, y= y-coordinate
+% theta = the pose angle.
+% SF = scaling factor.
+
+% Output Variables:
+% P = a matrix of 12x2
 % ========================================================================
+
 
 function P = pose2stance(x,y,theta,SF)
 
@@ -25,6 +34,8 @@ P(11,:) = [-0.25 -0.7];
 P(12,:) = [-0.25 -0.8];
 P = P*SF;
 
+% here it is now calculating the rotation matrix of the robot.
+% the returned matrix is of 12 x 2 size.
 for I=1:12
     
     P(I,:) = (R*(P(I,:))')';
